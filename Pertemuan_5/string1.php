d8b293ed2eb454e94177d9c573b0c5fbf2e1537f
Muhammad 'Athif Attaqy <athifattaqy24@gmail.com>
2024-03-19 13:32:53 +0700

    Pertemuan 5 - String

---
+++ Pertemuan_5/string1.php
@@ -0,0 +1,12 @@
+<?php //String
+$loremIpsum = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+    Minus quasi vitae, est assumenda adipisci nemo accusamus reprehenderit 
+    deserunt repudiandae? Maxime cupiditate magni iusto quam, corrupti 
+    autem modi beatae?.";
+    
+    echo "<p>{$loremIpsum}</p>";
+    echo "Panjang karakter : " .strlen($loremIpsum). "<br/";
+    echo "Panjang kata : " .str_word_count($loremIpsum). "<br/";
+    echo "<p>" .strtoupper($loremIpsum). "</p>";
+    echo "<p>" .strtolower($loremIpsum). "</p>";
+?>
