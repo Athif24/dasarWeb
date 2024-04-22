0b4bf2b9e0f12bf40f1828bafe0ea3a0e1ba4300
Athif24 <athifattaqy24@gmail.com>
2024-04-22 20:54:11 +0700

    Pertemuan 8 - Praktikum 4 (Menghias Upload File)

--- Pertemuan_8/upload.js
+++ Pertemuan_8/upload.js
@@ -1,21 +1,29 @@
-$(document).ready(function() {
-    $('#upload-form').submit(function(e) {
+$(document) .ready(function() {
+    $("#file") .change(function() {
+        if (this.file.lenght > 0) {
+            $("#upload-button") .prop("disabled", false) .css("opacity" , 1);
+        } else {
+            $("#upload-button") .prop("disabled", true) .css("opacity" , 0.5);
+        }
+    })
+    
+    $("#upload-form") .submit(function(e) {
         e.preventDefault();
 
         var formData = new FormData(this);
 
-        $.ajax ({
-            type: 'POST',
-            url: 'upload_ajax.php',
-            data: formData,
-            cache: false,
-            contentType: false,
-            processData: false,
-            success: function(response) {
-                $('#status').html(response);
+        $.ajax({
+            type : "POST",
+            url : "upload_ajax.php",
+            data : formData,
+            Cache : false,
+            contentType : false,
+            processData : false,
+            success : function(response) {
+                $("#status") . html(response);
             },
-            error: function() {
-                $('#status').html('Terjadi kesalahan saat mengunggah file.')
+            error : function() {
+                $("#status") .html("Terjadi kesalahan saat mengunggah file.");
             }
         })
     })
