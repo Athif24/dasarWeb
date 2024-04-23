c34e1ae20de660bacca1e8cf3d539aa5f09112ca
Athif24 <athifattaqy24@gmail.com>
2024-04-23 13:29:38 +0700

    Pertemuan 10 - Praktikum 4 (CRUD Bagian Delete)

--- Pertemuan_10/proses.php
+++ Pertemuan_10/proses.php
@@ -1,13 +1,13 @@
 <?php
     include("koneksi.php");
 
-    $aksi = $_GET["aksi"];
-    $nama = $_POST["nama"];
-    $jenis_kelamin = $_POST["jenis_kelamin"];
-    $alamat = $_POST["alamat"];
-    $no_telp = $_POST["no_telp"];
+    $aksi = $_GET['aksi'];
+    $nama = $_POST['nama'];
+    $jenis_kelamin = $_POST['jenis_kelamin'];
+    $alamat = $_POST['alamat'];
+    $no_telp = $_POST['no_telp'];
 
-    if($aksi == "tambah") {
+    if($aksi == 'tambah') {
         $query ="INSERT INTO anggota (nama, jenis_kelamin, alamat, no_telp) VALUES ('$nama', '$jenis_kelamin', '$alamat', '$no_telp')";
 
         if (mysqli_query($koneksi, $query)) {
@@ -15,10 +15,10 @@
             exit();
         } else {
             echo "Gagal menambahkan data : " . mysqli_error($koneksi);
-        }
-    } else if ($aksi == "ubah") {
-        if (isset($_POST["id"])) {
-            $id = $_POST["id"];
+        } 
+    } else if ($aksi == 'ubah') {
+        if (isset($_POST['id'])) {
+            $id = $_POST['id'];
 
             $query = "UPDATE anggota SET nama='$nama', jenis_kelamin='$jenis_kelamin', alamat='$alamat', no_telp='$no_telp' WHERE id='$id'";
             
@@ -50,5 +50,6 @@
         header("Location: index.php");
         exit();
     }
+
     mysqli_close($koneksi);
-?>;
+?>
